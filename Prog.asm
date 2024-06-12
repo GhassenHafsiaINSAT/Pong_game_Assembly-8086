0e51af24f122539ea7cf14b97c3c9caccf0af037
GhassenHafsiaINSAT <ghassen.hafsia@insat.ucar.tn>
2024-06-12 17:29:57 +0200

    creating our ball object and the segment setup
    the segment setup by pointing the segments to the registers and creating the ball object (position and adding it as a variable)

--- Prog.asm
+++ Prog.asm
@@ -3,11 +3,20 @@ STACK SEGMENT PARA STACK
 STACK ENDS ; S is for the SEGMENT
 
 DATA SEGMENT PARA 'DATA'
+	BALL_X DW 0Ah ; X position of the ball, DW stands for define word 16 bits
+	BALL_Y DW 0Ah ; Y position of the ball 
 DATA ENDS 
 
 CODE SEGMENT PARA 'CODE' 
 	MAIN PROC FAR ; FAR means that the call is far, it crosses segment boundaries.
-	
+	ASSUME CS:CODE,DS:DATA,SS:STACK ; assume as code, data and stack segments the respective registers.  
+	PUSH DS 						; push the DS segment to the stack
+	SUB AX,AX						; clean the AX register	
+	PUSH AX							; push the AX register to the stack 
+	MOV AX,DATA						; save on the AX register the content of the DATA segment 
+	MOV DS,AX 						; save the DS segment the content of AX register   
+	POP AX							; release the top item of the stack to the AX register  
+		
 		MOV AH,00h ; Set the configuration to video mode.  
 		MOV AL,13h ; Choose the video mode.    
 		INT 10h ; Execute the configuration.
@@ -20,8 +29,8 @@ CODE SEGMENT PARA 'CODE'
 		MOV AH,0Ch ; set the configuration to writing a pixel.
 		MOV AL,0Fh ; Choose white as color.
 		MOV BH,00h ; choose the page number.
-		MOV CX,0Ah ; Set the column (X).
-		MOV DX,0Ah ; set the line (Y).
+		MOV CX,BALL_X ; Set the column (X).
+		MOV DX,BALL_Y ; set the line (Y).
 		INT 10h ; execute the configuration.  
 		
 		RET ; RET is the return, the exit of the procedure.  
