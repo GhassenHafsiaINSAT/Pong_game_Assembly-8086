002e783d40b422b0e8f4f61015cf694d3ebf715b
GhassenHafsiaINSAT <ghassen.hafsia@insat.ucar.tn>
2024-06-12 15:27:26 +0200

    setting the background and drawing my first pixel
    to set the background, i set the video mode, the color of the background. I also chose the position and the color of the pixel

--- Prog.asm
+++ Prog.asm
@@ -6,11 +6,23 @@ DATA SEGMENT PARA 'DATA'
 DATA ENDS 
 
 CODE SEGMENT PARA 'CODE' 
-	MAIN PROC FAR 
+	MAIN PROC FAR ; FAR means that the call is far, it crosses segment boundaries.
 	
-		MOV DL,'A'
-		MOV AH,6h
-		INT 21h ; interruption by calling the 21 service.  
+		MOV AH,00h ; Set the configuration to video mode.  
+		MOV AL,13h ; Choose the video mode.    
+		INT 10h ; Execute the configuration.
+
+		MOV AH,0Bh ; Set the configuration 
+		MOV BH,00h ; to the background color.
+		MOV BL,00h ; set black as background color.
+		INT 10h ; Execute the configuration.
+
+		MOV AH,0Ch ; set the configuration to writing a pixel.
+		MOV AL,0Fh ; Choose white as color.
+		MOV BH,00h ; choose the page number.
+		MOV CX,0Ah ; Set the column (X).
+		MOV DX,0Ah ; set the line (Y).
+		INT 10h ; execute the configuration.  
 		
 		RET ; RET is the return, the exit of the procedure.  
 	MAIN ENDP ; P is for procedure.  
